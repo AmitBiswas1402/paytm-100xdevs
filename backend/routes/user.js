3d05c2032495ad995dfb2edbccaa31348a7c20a1
Amit Biswas <amit.142biswas@gmail.com>
2024-05-31 22:57:31 +0530

    100x

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const userRouter = require('./user')
+
 const router = express.Router()
 
-router.use("/user", userRouter)
+router.post("/signup")
 
 module.exports = router
