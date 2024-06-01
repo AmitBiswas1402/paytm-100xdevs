eb2fb97d8a19c4cc57f38d340212e5c9320596c8
Amit Biswas <amit.142biswas@gmail.com>
2024-06-01 22:15:36 +0530

    paytm

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,26 @@
 const express = require('express')
-
+const zod = require('zod')
 const router = express.Router()
 
-router.post("/signup")
+const signUpSchema = zod.object({
+    username: zod.string(),
+    password: zod.string(),
+    firstName: zod.string(),
+    password: zod.string(),
+})
+
+router.post("/signup", (req, res) => {
+    const body = req.body;
+    const {success} = signUpSchema.safeParse(req.body);
+    if (!success) {
+        return res.json({
+            message: "Email already taken / Incorrect input"
+        })
+    }
+
+    const user = User.findOne({
+        
+    })
+})
 
 module.exports = router
